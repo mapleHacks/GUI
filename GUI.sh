30a0f4a18ac212ee57f39d67db116e10011cc438
mapleHacks <64471798+mapleHacks@users.noreply.github.com>
2020-05-05 11:00:52 +0530

    Sometimes I forget GUI Commands

---
+++ GUI.sh
@@ -0,0 +1,8 @@
+apt update
+apt -y install vnc4server
+apt install xfce4 xfce4-goodies -y
+apt install fluxbox
+apt install mate-desktop-environment-core
+apt install firefox
+apt install falkon
+vncpasswd
